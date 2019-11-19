d65f18b13bc68326a363907fe6b5b87bf722b024
Jeff Eckert <bromodo@gmail.com>
2019-11-18 21:37:09 -0500

    first commit

---
+++ lib/brewries_ruby_cli_project.rb
@@ -0,0 +1,6 @@
+require "brewries_ruby_cli_project/version"
+
+module BrewriesRubyCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/brewries_ruby_cli_project/version.rb
@@ -0,0 +1,3 @@
+module BrewriesRubyCliProject
+  VERSION = "0.1.0"
+end
