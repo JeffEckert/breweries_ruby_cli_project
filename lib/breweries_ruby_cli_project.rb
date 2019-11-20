6389998fcb75cd28a73bcc13bd2a159b496bc68c
Jeff Eckert <bromodo@gmail.com>
2019-11-20 10:03:37 -0500

    setup structure of how app should look

--- lib/breweries_ruby_cli_project.rb
+++ lib/breweries_ruby_cli_project.rb
@@ -1,8 +1,5 @@
 require_relative "./breweries_ruby_cli_project/version"
 require_relative "./breweries_ruby_cli_project/cli"
 
-module BreweriesRubyCliProject
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
 
--- lib/breweries_ruby_cli_project/cli.rb
+++ lib/breweries_ruby_cli_project/cli.rb
@@ -1,8 +1,44 @@
-# CLI Controller
+
 class BreweriesRubyCliProject::CLI
 
     def call
-        "Welcome! Let's get you a beer."
+       puts "Welcome! Let's get you a beer."
+       initial_query
+       list_breweries
+       menu
+       brewery_details
     end
+
+    def initial_query
+        puts "Enter the state you would like to find a Brewery in"
+        # User will enter state
+    end
+
+
+    def list_breweries
+        #get local breweries based on state
+        puts <<-DOC
+        1.Local Brew
+        2.Stateline Brew
+        3.Narragansett 
+        DOC
+    end
+
+    def menu 
+        puts "Enter the number of the Brewery you would like to learn more about"
+    end
+
+    def brewery_details
+        puts <<-DOC
+        Local Brew
+        Address
+        Phone Number
+        website
+        DOC
+    end
+    
+
+    
+
 
 end
